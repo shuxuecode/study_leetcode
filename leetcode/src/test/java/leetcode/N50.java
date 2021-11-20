52cdcf6134b621cdf05adeb5fcabdc9b91ffc649
shuxuezhao <zhaoshuxue@163.com>
2021-11-20 15:24:51 +0800

    commitByZsx

---
+++ leetcode/src/test/java/leetcode/N50.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @date 2021/11/20
+ */
+public class N50 {
+    // todo
+    public double myPow(double x, int n) {
+        return Math.pow(x, n);
+    }
+
+
+    @Test
+    void t() {
+        System.out.println(myPow(2, 2));
+    }
+}
