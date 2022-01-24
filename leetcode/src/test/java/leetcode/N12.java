1dd2d56870b6c539d952429653ac6e8945af9c9c
shuxuecode <zhaoshuxue@163.com>
2022-01-24 22:17:25 +0800

    commitByZsx

---
+++ leetcode/src/test/java/leetcode/N12.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * todo
+ * @date 2022/1/24
+ */
+public class N12 {
+
+    int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    public String intToRoman(int num) {
+        StringBuffer roman = new StringBuffer();
+        for (int i = 0; i < values.length; ++i) {
+            int value = values[i];
+            String symbol = symbols[i];
+            while (num >= value) {
+                num -= value;
+                roman.append(symbol);
+            }
+            if (num == 0) {
+                break;
+            }
+        }
+        return roman.toString();
+    }
+
+
+    @Test void t(){
+        // 提示 ： 1 <= num <= 3999
+        System.out.println(intToRoman(111));
+        System.out.println(intToRoman(1111));
+        System.out.println(intToRoman(3999));
+    }
+
+}
