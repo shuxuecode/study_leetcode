7882b2c6eb584de7715f5b6ee2d989cdeda7fce9
shuxuecode <zhaoshuxue@163.com>
2022-02-18 18:56:36 +0800

    commitByZsx

---
+++ leetcode/src/test/java/leetcode999/N688.java
@@ -0,0 +1,17 @@
+package leetcode999;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @date 2022/2/18
+ */
+public class N688 {
+
+    public double knightProbability(int n, int k, int row, int column) {
+        return 0d;
+    }
+
+    @Test void t(){
+        // todo zsx
+    }
+}
